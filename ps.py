fd188cf22984884abe5b4a127e075c578de0080d
VanessaNwajei <vanicsam2005@gmail.com>
2024-10-28 18:51:37 +0100

    another partial sort

--- ps.py
+++ ps.py
@@ -13,3 +13,22 @@ k = 3
 
 smallest_elements = partial_sort(n,a,k)
 print(smallest_elements)
+
+'''
+
+OR
+
+'''
+
+def partial_sort_1(n,a,k):
+    if len(a)==n:
+        a.sort()
+        return a[:k]
+    
+
+n = 10
+a = [4, -6, 7, 8, -9, 100, 12, 13, 56, 17]
+k = 3
+
+smallest_elements = partial_sort_1(n,a,k)
+print(smallest_elements)
