b07022d9609523e1fe8a368aefdf33f97f404779
VanessaNwajei <vanicsam2005@gmail.com>
2024-10-28 17:55:18 +0100

    fib

--- fib.py
+++ fib.py
@@ -1,11 +1,13 @@
 def rabbits(n,k):
     #n : the number of months
-    #k : the number of rabbits pairs
-    if n <= 2:
-        return 1
-    else:
-        return rabbits(n-1, k) + k * rabbits(n-2, k)
-    
+    #k : the number of the pairs of rabbits
+    f1, f2 = 1, 1
+    for i in range(2,n):
+        current = f1 + k * f2
+        f2 = f1
+        f1 = current
+    return current
+
 
 n = 36
 k = 2
