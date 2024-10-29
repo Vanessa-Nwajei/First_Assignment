452057d9185b35470d5118868ac1c60f07dadf63
VanessaNwajei <vanicsam2005@gmail.com>
2024-10-29 14:37:03 +0100

    2 way partition

---
+++ par.py
@@ -0,0 +1,20 @@
+def two_way_partition(n, A_arr):
+    B_arr = []
+    first_element = A_arr[0]
+    smaller_than_first_element = []
+    larger_than_first_element = []
+
+    for i in A_arr:
+        if i < first_element:
+            smaller_than_first_element.append(i)
+        else:
+            larger_than_first_element.append(i)
+
+    B_arr = smaller_than_first_element + larger_than_first_element
+    return B_arr
+
+n = 9
+m = [7,2,5,6,1,3,9,4,8]
+partition = two_way_partition(n,m)
+
+print(partition)
