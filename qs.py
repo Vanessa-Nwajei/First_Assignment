9b23167a93be77ea239309845526e974bfb11295
VanessaNwajei <vanicsam2005@gmail.com>
2024-10-29 12:29:12 +0100

    quick sort

--- qs.py
+++ qs.py
@@ -2,8 +2,8 @@ def quick_sort(n,arr):
     if len(arr) == n:
         arr.sort()
         return arr
-   # else:
-        #raise ValueError('Lenght of array either too long or too short') 
+    else:
+        raise ValueError('Lenght of array either too long or too short') 
     
 n = 7
 a = [5, -2, 4, 7, 8, -10, 11]
