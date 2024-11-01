63728c60fb6c15fc119f1a23fe676ed10adc0925
VanessaNwajei <vanicsam2005@gmail.com>
2024-11-01 15:19:59 +0100

    RNA to protein

---
+++ PROT.py
@@ -0,0 +1,33 @@
+rna_codon_table = {
+    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+    'UAU': 'Y', 'UAC': 'Y', 'UAA': 'Stop', 'UAG': 'Stop',
+    'UGU': 'C', 'UGC': 'C', 'UGA': 'Stop', 'UGG': 'W',
+    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+}
+
+def translate_rna_to_protein(rna_string):
+    protein = []
+    # Process each codon (3 nucleotides)
+    for i in range(0, len(rna_string), 3):
+        codon = rna_string[i:i+3]
+        amino_acid = rna_codon_table.get(codon, 'Stop')
+        if amino_acid == 'Stop':
+            break
+        protein.append(amino_acid)
+    
+    return ''.join(protein)
+
+rna = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
+print(translate_rna_to_protein(rna))
